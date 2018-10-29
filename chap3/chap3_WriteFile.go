426c528d87293b6237844300e1b3c62e5d760d29
junya <hatiro7code@gmail.com>
2018-10-30 00:20:07 +0900

    20161029 WriteFile io.Copy

---
+++ chap3/chap3_WriteFile.go
@@ -0,0 +1,21 @@
+package main
+import (
+    "os"
+    "io"
+)
+// os.File構造体を使ってファイルからの入力をする
+// os.Create(), os.Open()はos.OpenFileという関数のエイリアスで、同じシステムコールが呼ばれている
+func main() {
+    file, err := os.Open("chap3_WriteFile.go")
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()  // ファイルを一度開いたらcloseする必要ある
+    io.Copy(os.Stdout, file)  // 標準出力にファイルの内容を全て書き出す
+    file, err = os.Open("WriteFile.go")
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()  // ファイルを一度開いたらcloseする必要ある
+    io.Copy(os.Stdout, file)  // 標準出力にファイルの内容を全て書き出す
+}
