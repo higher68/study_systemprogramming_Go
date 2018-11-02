100da397f1fcddb5b16c70853df49b6fac8aed01
junya <hatiro7code@gmail.com>
2018-11-02 23:10:55 +0900

    20181102 writefile differ comment added

--- chap3/chap3_WriteFile.go
+++ chap3/chap3_WriteFile.go
@@ -11,6 +11,7 @@ func main() {
         panic(err)
     }
     defer file.Close()  // ファイルを一度開いたらcloseする必要ある
+    // deferは現在のスコープが終了したらその後ろに書いてある行の処理を実行する
     io.Copy(os.Stdout, file)  // 標準出力にファイルの内容を全て書き出す
     file, err = os.Open("WriteFile.go")
     if err != nil {
