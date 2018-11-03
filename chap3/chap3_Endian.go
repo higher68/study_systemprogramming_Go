7c7778f7890112b6229e369f2e02951f1ea79d26
junya <hatiro7code@gmail.com>
2018-11-03 16:26:23 +0900

    20181103 endian transform wrote

--- chap3/chap3_Endian.go
+++ chap3/chap3_Endian.go
@@ -9,4 +9,11 @@ import (
 // リトルエンディアンでは、1000という数値があったとき、小さい桁からメモリに格納する
 // ビッグエンディアン：大きい桁からメモリに格納される(ネットワークバイトオーダー)とも呼ぶ
 func main() {
+    // 32ビットのビッグエンディアンのデータ(10000)
+    data := []byte{0x0, 0x0, 0x27, 0x10}
+    var i int32
+    // エンディアン変換
+    binary.Read(bytes.NewReader(data), binary.BigEndian, &i)  // dataを読んでbigendianが変換されてiに格納される。
+    fmt.Printf("data: %d\n", i)
+}
     
