2e92f2f1b88e68cfcc63d776b825cc8f727832db
junya <hatiro7code@gmail.com>
2018-11-17 23:14:08 +0900

    20181115 read & write add

--- chap3/chap3_q1.go
+++ chap3/chap3_q1.go
@@ -1,13 +1,22 @@
 package main
 import (
     "io"
-    "fmt"
+    "io/ioutil"
     "os"
 )
 func main() {
-    buffer := make([bytes])
-    os.Stdin.Read(buffer)
-    file, err := os.Create("new.txt")
+    buffer := make([]byte, 1024) // 読み込むデータの格納場所を事前に用意
+    filename, err := os.Stdin.Read(buffer)  // バッファに格納
+    if err != nil{
+        panic(err)
+    }
+    // bufferは一時的にデータをためておく場所
+    file,err := os.Open(string(filename))
+    if err != nil{
+        panic(err)
+    }
+    buffer2, err :=  ioutil.ReadAll(file)  // ioutilはバッファを制御できないのでで、ちょい危険
+    file_out, err := os.Create("new.txt")
+    io.Copy(file_out, string(buffer2))
     defer file.close()
-    io.Copy(os.Stdout, file)
 }
