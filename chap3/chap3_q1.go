7dddc72788a6c2f12cdf2817272004c43d3bc60d
junya <hatiro7code@gmail.com>
2018-11-18 21:36:32 +0900

    20181117 wrote

--- chap3/chap3_q1.go
+++ chap3/chap3_q1.go
@@ -3,20 +3,35 @@ import (
     "io"
     "io/ioutil"
     "os"
+    "fmt"
+    // "strings"
 )
 func main() {
-    buffer := make([]byte, 1024) // 読み込むデータの格納場所を事前に用意
-    filename, err := os.Stdin.Read(buffer)  // バッファに格納
-    if err != nil{
-        panic(err)
+    filename := ""
+    for {buffer := make([]byte, 1) // 読み込むデータの格納場所を事前に用意
+        size, err := os.Stdin.Read(buffer)  // バッファに格納
+        if string(buffer) == "\n"{
+            fmt.Println(err)
+            break
+        }
+        filename += string(buffer)
+        fmt.Println(size)
     }
+    fmt.Println(filename)
+    // fmt.Println("hoge")
+    // fmt.Println(string(buffer))
+    //fmt.Println("chap3_q1.go")
+    // fmt.Println(strings.TrimRight(string(buffer), "\n"))
+    // fmt.Println(len(strings.TrimRight(string(buffer), "\n")))
     // bufferは一時的にデータをためておく場所
-    file,err := os.Open(string(filename))
+    // filename := string(buffer)
+    // file,err := os.Open(strings.TrimRight(filename, "\n"))
+    file,err := os.Open(filename)
     if err != nil{
         panic(err)
     }
     buffer2, err :=  ioutil.ReadAll(file)  // ioutilはバッファを制御できないのでで、ちょい危険
     file_out, err := os.Create("new.txt")
-    io.Copy(file_out, string(buffer2))
-    defer file.close()
+    io.WriteString(file_out, string(buffer2))
+    // defer file_out.close()
 }
